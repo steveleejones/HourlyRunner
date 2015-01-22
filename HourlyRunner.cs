7824dd87c4bb8f85acf460bdf4b090236baf6a3f
Steve Jones <steveleejones@gmail.com>
2015-01-22 12:19:10 +0000

    initial commit
    Initial Commit

---
+++ HourlyRunner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CoreDataLibrary;
+
+namespace HourlyRunner
+{
+    public class HourlyRunner : BaseCoreDataWorkItem
+    {
+        public override string Name
+        {
+            get { return "Hourly Runner"; }
+        }
+
+        public override bool DueToRun()
+        {
+            ReadFromDb();
+            var dateTime = DateTime.Now;
+            var timeSpan = dateTime - LastRun;
+
+            if (timeSpan.TotalHours >= 1)
+            {
+                LastRun = dateTime;
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Run()
+        {
+            if (DueToRun())
+            {
+                ReportLogger.AddMessage("Hourly Runner", "Run called");
+            }
+            return true;
+        }
+    }
+}
